2bf697d8eda049f55d3cc142a23ad024d8ca63ec
anonrst <mdayan1qaz@gmail.com>
2025-11-01 08:07:02 +0000

    bitwise opearator and solved two question

---
+++ bitwise/code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+bool isPowerOfTwo(int x){
+    if((x & 2 )== 0) return true ;
+return false;
+}
+int main(){
+int number = 32;
+bool isITOwer  = isPowerOfTwo(number);
+cout << isITOwer << endl;
+    return 0;
+}
